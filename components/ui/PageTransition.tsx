73b689b4ed998e7a6bdd89c1ae623923c786194a
Juan Leal Aliaga <100473223@alumnos.uc3m.es>
2026-07-04 20:42:52 +0200

    Fix unused import in PageTransition

--- components/ui/PageTransition.tsx
+++ components/ui/PageTransition.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { AnimatePresence, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { usePathname } from 'next/navigation'
 
 export default function PageTransition({ children }: { children: React.ReactNode }) {
